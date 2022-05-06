f1ef32f9e48373ddb7e8b162f3832b29b05e1565
MaxKnyazev <max.knyazev77@gmail.com>
2022-05-06 18:29:33 +0300

    feat: markup of all elements

---
+++ data/en.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-multi-spaces */
+/* eslint-disable object-curly-newline *//* eslint-disable linebreak-style */
+/* eslint-disable object-property-newline */
+/* eslint-disable linebreak-style */
+
+// eslint-disable-next-line no-undef
+const en = [
+  { keycode: 'Backquote',     title: '`',         value: '`',   shift: '~',   caps: '`',   classes: ['key'] },
+  { keycode: 'Digit1',        title: '1',         value: '1',   shift: '!',   caps: '1',   classes: ['key'] },
+  { keycode: 'Digit2',        title: '2',         value: '2',   shift: '@',   caps: '2',   classes: ['key'] },
+  { keycode: 'Digit3',        title: '3',         value: '3',   shift: '#',   caps: '3',   classes: ['key'] },
+  { keycode: 'Digit4',        title: '4',         value: '4',   shift: '$',   caps: '4',   classes: ['key'] },
+  { keycode: 'Digit5',        title: '5',         value: '5',   shift: '%',   caps: '5',   classes: ['key'] },
+  { keycode: 'Digit6',        title: '6',         value: '6',   shift: '^',   caps: '6',   classes: ['key'] },
+  { keycode: 'Digit7',        title: '7',         value: '7',   shift: '&',   caps: '7',   classes: ['key'] },
+  { keycode: 'Digit8',        title: '8',         value: '8',   shift: '*',   caps: '8',   classes: ['key'] },
+  { keycode: 'Digit9',        title: '9',         value: '9',   shift: '(',   caps: '9',   classes: ['key'] },
+  { keycode: 'Digit0',        title: '0',         value: '0',   shift: ')',   caps: '0',   classes: ['key'] },
+  { keycode: 'Minus',         title: '-',         value: '-',   shift: '_',   caps: '-',   classes: ['key'] },
+  { keycode: 'Equal',         title: '=',         value: '=',   shift: '+',   caps: '=',   classes: ['key'] },
+  { keycode: 'Backspace',     title: 'Backspace', value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn', 'key__fn-big'] },
+
+  { keycode: 'Tab',           title: 'Tab',       value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'KeyQ',          title: 'q',         value: 'q',   shift: 'Q',   caps: 'Q',   classes: ['key'] },
+  { keycode: 'KeyW',          title: 'w',         value: 'w',   shift: 'W',   caps: 'W',   classes: ['key'] },
+  { keycode: 'KeyE',          title: 'e',         value: 'e',   shift: 'E',   caps: 'E',   classes: ['key'] },
+  { keycode: 'KeyR',          title: 'r',         value: 'r',   shift: 'R',   caps: 'R',   classes: ['key'] },
+  { keycode: 'KeyT',          title: 't',         value: 't',   shift: 'T',   caps: 'T',   classes: ['key'] },
+  { keycode: 'KeyY',          title: 'y',         value: 'y',   shift: 'Y',   caps: 'Y',   classes: ['key'] },
+  { keycode: 'KeyU',          title: 'u',         value: 'u',   shift: 'U',   caps: 'U',   classes: ['key'] },
+  { keycode: 'KeyI',          title: 'i',         value: 'i',   shift: 'I',   caps: 'I',   classes: ['key'] },
+  { keycode: 'KeyO',          title: 'o',         value: 'o',   shift: 'O',   caps: 'O',   classes: ['key'] },
+  { keycode: 'KeyP',          title: 'p',         value: 'p',   shift: 'P',   caps: 'P',   classes: ['key'] },
+  { keycode: 'BracketLeft',   title: '[',         value: '[',   shift: '{',   caps: '[',   classes: ['key'] },
+  { keycode: 'BracketRight',  title: ']',         value: ']',   shift: '}',   caps: ']',   classes: ['key'] },
+  { keycode: 'Backslash',     title: '\\',        value: '\\',  shift: '|',   caps: '\\',  classes: ['key'] },
+  { keycode: 'Delete',        title: 'Del',       value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+
+  { keycode: 'CapsLock',      title: 'CapsLock',  value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn', 'key__fn-big'] },
+  { keycode: 'KeyA',          title: 'a',         value: 'a',   shift: 'A',   caps: 'A',   classes: ['key'] },
+  { keycode: 'KeyS',          title: 's',         value: 's',   shift: 'S',   caps: 'S',   classes: ['key'] },
+  { keycode: 'KeyD',          title: 'd',         value: 'd',   shift: 'D',   caps: 'D',   classes: ['key'] },
+  { keycode: 'KeyF',          title: 'f',         value: 'f',   shift: 'F',   caps: 'F',   classes: ['key'] },
+  { keycode: 'KeyG',          title: 'g',         value: 'g',   shift: 'G',   caps: 'G',   classes: ['key'] },
+  { keycode: 'KeyH',          title: 'h',         value: 'h',   shift: 'H',   caps: 'H',   classes: ['key'] },
+  { keycode: 'KeyJ',          title: 'j',         value: 'j',   shift: 'J',   caps: 'J',   classes: ['key'] },
+  { keycode: 'KeyK',          title: 'k',         value: 'k',   shift: 'K',   caps: 'K',   classes: ['key'] },
+  { keycode: 'KeyL',          title: 'l',         value: 'l',   shift: 'L',   caps: 'L',   classes: ['key'] },
+  { keycode: 'Semicolon',     title: ';',         value: ';',   shift: ':',   caps: ';',   classes: ['key'] },
+  { keycode: 'Quote',         title: '\'',        value: '\'',  shift: '"',   caps: '\'',  classes: ['key'] },
+  { keycode: 'Enter',         title: 'Enter',     value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn', 'key__fn-big'] },
+
+  { keycode: 'ShiftLeft',     title: 'Shift',     value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn', 'key__fn-big'] },
+  { keycode: 'KeyZ',          title: 'z',         value: 'z',   shift: 'Z',   caps: 'Z',   classes: ['key'] },
+  { keycode: 'KeyX',          title: 'x',         value: 'x',   shift: 'X',   caps: 'X',   classes: ['key'] },
+  { keycode: 'KeyC',          title: 'c',         value: 'c',   shift: 'C',   caps: 'C',   classes: ['key'] },
+  { keycode: 'KeyV',          title: 'v',         value: 'v',   shift: 'V',   caps: 'V',   classes: ['key'] },
+  { keycode: 'KeyB',          title: 'b',         value: 'b',   shift: 'B',   caps: 'B',   classes: ['key'] },
+  { keycode: 'KeyN',          title: 'n',         value: 'n',   shift: 'N',   caps: 'N',   classes: ['key'] },
+  { keycode: 'KeyM',          title: 'm',         value: 'm',   shift: 'M',   caps: 'M',   classes: ['key'] },
+  { keycode: 'Comma',         title: ',',         value: ',',   shift: '<',   caps: ',',   classes: ['key'] },
+  { keycode: 'Period',        title: '.',         value: '.',   shift: '>',   caps: '.',   classes: ['key'] },
+  { keycode: 'Slash',         title: '/',         value: '/',   shift: '?',   caps: '/',   classes: ['key'] },
+  { keycode: 'ArrowUp',       title: '&lt;',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'ShiftRight',    title: 'Shift',     value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn', 'key__fn-big'] },
+
+  { keycode: 'ControlLeft',   title: 'Ctrl',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'MetaLeft',      title: 'Win',       value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'AltLeft',       title: 'Alt',       value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'Space',         title: '',          value: ' ',   shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'AltRight',      title: 'Alt',       value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'ArrowLeft',     title: '&lt;',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'ArrowDown',     title: '&gt;',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'ArrowRight',    title: '&gt;',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+  { keycode: 'ControlRight',  title: 'Ctrl',      value: '',    shift: '',    caps: '',    classes: ['key', 'key__fn'] },
+];
+
+export default en;
--- script.js
+++ script.js
@@ -1,3 +1,115 @@
+/* eslint-disable import/extensions */
 /* eslint linebreak-style: ["error", "windows"] */
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
+
+import en from './data/en.js';
+import ru from './data/ru.js';
+
+console.log('App started...');
+
+let keyArr;
+switch (localStorage.getItem('language')) {
+  case 'en':
+    keyArr = en;
+    break;
+  case 'ru':
+    keyArr = ru;
+    break;
+
+  default:
+    keyArr = en;
+    localStorage.setItem('language', 'en');
+    break;
+}
+
+const btnRu = document.querySelector('.ru');
+const btnEn = document.querySelector('.en');
+const textarea = document.querySelector('.textarea');
+console.log(textarea);
+console.log(textarea.selectionStart);
+
+
+console.log(en);
+console.log(ru);
+
+// const keyA = document.getElementById('KeyA');'
+// keyA.children[0].classList.toggle('red');
+// console.log(keyA.children[0].innerText);
+
+const keyMap = new Map();
+// eslint-disable-next-line no-restricted-syntax
+for (const elem of keyArr) {
+  keyMap.set(elem.keycode, document.querySelector(`[data-keycode='${elem.keycode}']`));
+  // console.log(document.querySelector(`[data-keycode='${code}']`));
+}
+console.log(keyMap);
+
+// console.log(document.querySelector(`[data-keycode="${keyCodesArr[0]}"]`));
+
+// const keyMap = new Map();
+// keyMap.set('KeyA', KeyA);
+// keyMap.set('KeyB', KeyB);
+// keyMap.set('Backspace', Backspace);
+
+btnEn.addEventListener('click', () => {
+  if (localStorage.getItem('language') !== 'en') {
+    localStorage.setItem('language', 'en');
+    btnEn.classList.toggle('key__lang--active');
+    btnRu.classList.toggle('key__lang--active');
+  }
+});
+
+btnRu.addEventListener('click', () => {
+  if (localStorage.getItem('language') !== 'ru') {
+    localStorage.setItem('language', 'ru');
+    btnEn.classList.toggle('key__lang--active');
+    btnRu.classList.toggle('key__lang--active');
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  event.preventDefault();
+  const currentKey = keyMap.get(event.code);
+
+  console.log(event.code, 'keydown-->', event.key);
+  console.log(event);
+  console.log(currentKey);
+  console.log(keyArr[currentKey.dataset.index]);
+  console.log(keyArr[currentKey.dataset.index].value);
+  console.log(keyArr[currentKey.dataset.index].shift);
+  console.log(keyArr[currentKey.dataset.index].caps);
+  console.log(textarea.selectionStart);
+
+ 
+  textarea.innerHTML += keyArr[currentKey.dataset.index].value;
+
+  // currentKey.classList.toggle('key--press');
+  currentKey.classList.add('key--press');
+
+
+ 
+ 
+});
+
+document.addEventListener('keyup', (event) => {
+  event.preventDefault();
+
+  console.log(event.code, 'keyup-->', event.key); 
+
+  // if (event.code === 'KeyA') {
+  //   console.dir(event);
+  //   KeyA.classList.remove('char--press');
+  //   // console.dir(this);
+  // }
+  const currentKey = keyMap.get(event.code);
+  // currentKey.classList.toggle('key--press');
+  currentKey.classList.remove('key--press');
+});
+
+// document.addEventListener('keydown', function(event) {
+//   if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
+//     console.dir(event);
+//     console.dir(this);
+//   }
+// });
